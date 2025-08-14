3654697ebbd3dc62f1d6c12ac6610d6d23240a9f
evan54 <user@gmail.com>
2025-08-14 13:16:33 +0300

    theoretically final fix of reactflow.js

--- reactflow.js
+++ reactflow.js
@@ -1,5 +1,8 @@
 import {
-  useCallback
+  useCallback,
+  useState,
+  useRef,
+  useEffect,
 } from 'react';
 import { 
   ReactFlow,
@@ -7,11 +10,92 @@ import {
   Controls,
   Background,
   MiniMap,
+  MarkerType,
   applyNodeChanges,
   applyEdgeChanges,
+  useNodesState,
+  useEdgesState,
   addEdge,
 } from '@xyflow/react';
 
+export function render({ model }) {
+
+  console.log("rendering...")
+  const [py_nodes, setPyNodes] = model.useState('nodes');
+  const [py_edges, setPyEdges] = model.useState('edges');
+  const [defaultEdgeOptions] = model.useState('default_edge_options');
+
+  // Helper functions
+  const onNodesChange = useCallback(
+    (changes) => setPyNodes((nds) => {
+      const updated = applyNodeChanges(changes, nds);
+      // console.log('Update nodes:', updated);
+      return updated;
+    }),
+    [setPyNodes],
+  );
+
+  const onEdgesChange = useCallback(
+    (changes) =>
+      setPyEdges((eds) => {
+        const updated = applyEdgeChanges(changes, eds);
+        // console.log('Updated edges:', updated);
+        return updated;
+      }),
+    [setPyEdges],
+  );
+
+  // Goal here is to update the python state with the selected nodes
+  /*const onSelectionChange = useCallback(
+    ({ nodes: selected_nodes, edges: selected_edges }) => {
+      const selectedNodeIds = new Set(selected_nodes.map(n => n.id));
+      model.send_msg("update_selected_nodes", selectedNodeIds)
+    }, []
+  );
+
+  // Update python when a node is no longer dragged
+  const onNodeDragStop = (event, ...changes) => {
+    applyNodeChanges(changes, py_nodes)
+  }
+  */
+
+  const onConnect = useCallback((connection) => {
+    setPyEdges((eds) => addEdge(connection, eds));
+    // model.send_msg(['edge_connect', connection ]);
+  }, [addEdge, model]);
+
+  // Render the external React component with props
+  return (
+    <div style={{ width: '100%', height: '100%' }}>
+      <ReactFlowProvider>
+        <ReactFlow 
+         nodes={py_nodes}
+         edges={py_edges}
+         defaultEdgeOptions={defaultEdgeOptions}
+
+         // nodes
+         onNodesChange={onNodesChange}
+         // onNodeDrag={onNodesChange}
+         // onNodeDragStop={onNodeDragStop}
+
+         // edges
+         onEdgesChange={onEdgesChange}
+         onConnect={onConnect}
+
+         // other
+         // onSelectionChange={onSelectionChange}
+        >
+
+          <Controls />
+          <MiniMap />
+          <Background />
+        </ReactFlow>
+      </ReactFlowProvider>
+    </div>
+  );
+}
+
+  /*
 
 function serializeMouseEvent(e) {
   return {
@@ -33,65 +117,26 @@ function serializeMouseEvent(e) {
   };
 }
 
-
-export function render({ model }) {
-  const [nodes, setNodes] = model.useState("nodes");
-  const [edges, setEdges] = model.useState("edges");
-
-  const onNodesChange = useCallback((changes) => {
-    setNodes((nds) => applyNodeChanges(changes, nds));
-    // model.send_msg({ type: "nodes_change", changes });
-  }, [setNodes, model]);
-
   const genericMouseEvent = (type) => (event, ...args) => {
-    model.send_msg([type, [serializeMouseEvent(event), args]]);
+    console.log(type)
+    console.log(event, args)
+    setPyNodes((nds) => {
+      const next = applyNodeChanges(args, nds);
+      return next;
+    });
   }
 
   const genericHandler = (type) => (...args) => {
     // works
     const safeArgs = args.map(a => {
       try {
-        return JSON.parse(JSON.stringify(a, getCircularReplacer()));
+        return JSON.parse(JSON.stringify(a));
       } catch {
-        return String(a); // Fallback if still not serializable
+        return String(a);
       }
     });
     model.send_msg([type, safeArgs]);
   };
+  */
 
-  const onConnect = useCallback((connection) => {
-    setEdges((eds) => addEdge(connection, eds));
-    model.send_msg(['edge_connect', connection ]);
-  }, [addEdge, model]);
 
-  const onEdgesChange = (changes) => {
-    setEdges((eds) => applyEdgeChanges(changes, eds));
-    model.send_msg(['edges_change', changes]);
-  };
-
-  // Render the external React component with props
-  return (
-    <div style={{ width: '100%', height: '100%' }}>
-      <ReactFlowProvider>
-        <ReactFlow 
-         nodes={nodes}
-         edges={edges}
-
-         // nodes
-         onNodesChange={onNodesChange}
-         onNodeClick={genericMouseEvent('node_click')}
-         onNodeDragStop={genericMouseEvent('node_drag_stop')}
-
-         // edges
-         onEdgesChange={onEdgesChange}
-         onConnect={onConnect}
-        >
-
-          <Controls />
-          <MiniMap />
-          <Background />
-        </ReactFlow>
-      </ReactFlowProvider>
-    </div>
-  );
-}
