0586be5abcbab9ba23ad4769e71cd9c6dda73c09
evan54 <user@gmail.com>
2025-08-15 12:06:48 +0300

    don't upate python when dragging nodes only when stopping

--- src/reactflow.js
+++ src/reactflow.js
@@ -11,81 +11,65 @@ import {
   Background,
   MiniMap,
   MarkerType,
-  applyNodeChanges,
-  applyEdgeChanges,
+  // applyNodeChanges,
+  // applyEdgeChanges,
+  addEdge,
   useNodesState,
   useEdgesState,
-  addEdge,
 } from '@xyflow/react';
 
 export function render({ model }) {
 
-  console.log("rendering...")
+  // console.log("rendering...")
   const [py_nodes, setPyNodes] = model.useState('nodes');
   const [py_edges, setPyEdges] = model.useState('edges');
   const [defaultEdgeOptions] = model.useState('default_edge_options');
+  const [nodes, setNodes, onNodesChange] = useNodesState(py_nodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(py_edges);
 
-  // Helper functions
-  const onNodesChange = useCallback(
-    (changes) => setPyNodes((nds) => {
-      const updated = applyNodeChanges(changes, nds);
-      // console.log('Update nodes:', updated);
-      return updated;
-    }),
-    [setPyNodes],
-  );
+  // ref for latest nodes and edges
+  const nodesRef = useRef(nodes);
+  const edgesRef = useRef(edges);
 
-  const onEdgesChange = useCallback(
-    (changes) =>
-      setPyEdges((eds) => {
-        const updated = applyEdgeChanges(changes, eds);
-        // console.log('Updated edges:', updated);
-        return updated;
-      }),
-    [setPyEdges],
-  );
+  // sync current value
+  useEffect(() => { nodesRef.current = nodes; }, [nodes]);
+  useEffect(() => { edgesRef.current = edges; }, [edges]);
 
-  // Goal here is to update the python state with the selected nodes
-  /*const onSelectionChange = useCallback(
-    ({ nodes: selected_nodes, edges: selected_edges }) => {
-      const selectedNodeIds = new Set(selected_nodes.map(n => n.id));
-      model.send_msg("update_selected_nodes", selectedNodeIds)
-    }, []
-  );
+  // sync changes from python to local
+  useEffect(() => { setNodes(py_nodes); }, [py_nodes, setNodes]);
+  useEffect(() => { setEdges(py_edges); }, [py_edges, setEdges]);
 
-  // Update python when a node is no longer dragged
-  const onNodeDragStop = (event, ...changes) => {
-    applyNodeChanges(changes, py_nodes)
-  }
-  */
+  // Sync change from local to python
+  const syncToPython = useCallback(() => {
+    setPyNodes(nodesRef.current);
+    setPyEdges(edgesRef.current);
+  }, [setPyNodes, setPyEdges]);
 
+  // onConnect needs to be handled specially as it modifies state directly
   const onConnect = useCallback((connection) => {
-    setPyEdges((eds) => addEdge(connection, eds));
-    // model.send_msg(['edge_connect', connection ]);
-  }, [addEdge, model]);
+    const newEdges = addEdge(connection, edgesRef.current);
+    setEdges(newEdges);
+    setPyEdges(newEdges);
+  }, [setEdges, setPyEdges]);
 
   // Render the external React component with props
   return (
     <div style={{ width: '100%', height: '100%' }}>
       <ReactFlowProvider>
         <ReactFlow 
-         nodes={py_nodes}
-         edges={py_edges}
-         defaultEdgeOptions={defaultEdgeOptions}
+          nodes={nodes}
+          edges={edges}
+          defaultEdgeOptions={defaultEdgeOptions}
 
-         // nodes
-         onNodesChange={onNodesChange}
-         // onNodeDrag={onNodesChange}
-         // onNodeDragStop={onNodeDragStop}
+          // Local state
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
 
-         // edges
-         onEdgesChange={onEdgesChange}
-         onConnect={onConnect}
-
-         // other
-         // onSelectionChange={onSelectionChange}
+          // Sync state
+          onNodeDragStop={syncToPython}
+          onSelectionChange={syncToPython}
+          onConnect={onConnect}
         >
-
           <Controls />
           <MiniMap />
           <Background />
@@ -94,49 +78,3 @@ export function render({ model }) {
     </div>
   );
 }
-
-  /*
-
-function serializeMouseEvent(e) {
-  return {
-    altKey: e.altKey,
-    button: e.button,
-    buttons: e.buttons,
-    clientX: e.clientX,
-    clientY: e.clientY,
-    ctrlKey: e.ctrlKey,
-    metaKey: e.metaKey,
-    movementX: e.movementX,
-    movementY: e.movementY,
-    pageX: e.pageX,
-    pageY: e.pageY,
-    relatedTarget: e.relatedTarget ? String(e.relatedTarget) : null,
-    screenX: e.screenX,
-    screenY: e.screenY,
-    shiftKey: e.shiftKey
-  };
-}
-
-  const genericMouseEvent = (type) => (event, ...args) => {
-    console.log(type)
-    console.log(event, args)
-    setPyNodes((nds) => {
-      const next = applyNodeChanges(args, nds);
-      return next;
-    });
-  }
-
-  const genericHandler = (type) => (...args) => {
-    // works
-    const safeArgs = args.map(a => {
-      try {
-        return JSON.parse(JSON.stringify(a));
-      } catch {
-        return String(a);
-      }
-    });
-    model.send_msg([type, safeArgs]);
-  };
-  */
-
-
